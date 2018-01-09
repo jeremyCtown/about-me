23fa189048d54587c438bbbed927b7addc541dd8
Jeremy Crawford <34947155+JeremyCtown@users.noreply.github.com>
2018-01-09 14:24:11 -0800

    added question 2 to JS and created initial stylesheet

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var playTheGame = prompt('Hi! Welcome to my page! Do you want to play a game? Reply with Y of N.').toUpperCase();
-console.log("User entered" + playTheGame);
+console.log("User entered " + playTheGame);
 
 /* PSEUDO CODE
 
@@ -19,4 +19,15 @@ if(playTheGame === 'Y') {
     alert('That\'s too bad. Let\'s play anyway!');
 } else {
     alert('You little rebel :) On to the game!');
+}
+
+var jeremyAge = prompt('Question 1 - Am I 31 years old? Reply Y or N.').toUpperCase();
+console.log("User entered " + jeremyAge);
+
+if(jeremyAge === 'Y') {
+    alert('I wish! I\'m 33 years old.');
+} else if (jeremyAge === 'N') {
+    alert('Good guess! I\'m 33 years young');
+} else {
+    alert('Sometimes its best to not answer ;) I\'m 33.');
 }
