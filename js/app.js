ad0b6ff10ad2ddb319416c8b14094f7a9ada91a1
Jeremy Crawford <34947155+JeremyCtown@users.noreply.github.com>
2018-01-10 13:23:57 -0800

    all edits for Day 2 resubmission

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var playTheGame = prompt('Hi! Welcome to my page! Do you want to play a game? Reply with Y of N.').toUpperCase();
-console.log("User entered " + playTheGame);
+var userName = prompt('Hi! Welcome to my page! What is your name?');
+console.log('User is named ' + userName);
+
+var playTheGame = prompt('Hi! Welcome to my page, ' + userName + '! Do you want to play a game? Reply with Y of N.').toUpperCase();
+console.log('Did ' + userName + ' choose to play the game? ' + playTheGame);
 
 /* PSEUDO CODE
 
@@ -14,64 +17,64 @@ if (the user enters Y or YES) {
 } */
 
 if(playTheGame === 'Y' || playTheGame === 'YES') {
-    alert('Thanks for playing! Let\'s go.');
+    alert('Thanks for playing, ' + userName + '! Let\'s go.');
 } else if (playTheGame === 'N' || playTheGame === 'NO') {
-    alert('That\'s too bad. Let\'s play anyway!');
+    alert('That\'s too bad, ' + userName + '. Let\'s play anyway!');
 } else {
-    alert('You little rebel :) On to the game!');
+    alert('Oh ' + userName + ', you little rebel :) On to the game!');
 }
 
 var jeremyAge = prompt('The game is guessing facts about me. First, am I 31 years old? Reply Y or N.').toUpperCase();
-console.log("User entered " + jeremyAge);
+console.log('Did ' + userName + ' think I am 31? '+ jeremyAge);
 
 if(jeremyAge === 'Y' || jeremyAge === 'YES') {
-    alert('I wish! I\'m 33 years old. Next question');
+    alert('I wish ' + userName + '! I\'m 33 years old. Next question');
 } else if (jeremyAge === 'N') {
-    alert('Good guess! I\'m 33 years young. Next question');
+    alert('Good guess, ' + userName + '! I\'m 33 years young. Next question');
 } else {
-    alert('Sometimes it\'s best not to answer ;) I\'m 33. Next question');
+    alert('Good call, ' + userName + '. Sometimes it\'s best not to answer ;) I\'m 33. Next question');
 }
 
-var jeremyHome =  prompt('Now you know that I\'m 33 years old. Am I originally from the Pacific Northwest? Reply Y or N.').toUpperCase();
-console.log("User entered " + jeremyHome);
+var jeremyHome =  prompt('Well, ' + userName + ', now you know that I\'m 33 years old. Am I originally from the Pacific Northwest? Reply Y or N.').toUpperCase();
+console.log('Did ' + userName + ' guess that I am from the PNW? ' + jeremyHome);
 
 if(jeremyHome === 'Y' || jeremyHome === 'YES') {
-    alert('That would be cool, but unfortunately no. I was born in California but lived most of my life on the East Coast and in Germany. Moving on!');
+    alert('That would be cool, ' + userName + ', but unfortunately no. I was born in California but lived most of my life on the East Coast and in Germany. Moving on!');
 } else if (jeremyHome === 'N' || jeremyHome === 'NO') {
-    alert('You\'re right! I was born in California but spent most of my life on the East Coast and in Germany. Moving on!');
+    alert('You\'re right, ' + userName + '! I was born in California but spent most of my life on the East Coast and in Germany. Moving on!');
 } else {
-    alert('I get it, geography is hard so you didn\'t have an answer. I\'m Cali-born, East Coast and Germany raised. Moving on!');
+    alert('I get it, ' + userName + ', geography is hard so you didn\'t have an answer. I\'m Cali-born, East Coast and Germany raised. Moving on!');
 }
 
-var jeremySchool =  prompt('You know I\'m 33 and that I\'m a PNW transplant. Did I attend the University of Washington? Reply Y or N.').toUpperCase();
-console.log("User entered " + jeremySchool);
+var jeremySchool =  prompt(userName + ', now you know I\'m 33 and that I\'m a PNW transplant. Did I attend the University of Washington? Reply Y or N.').toUpperCase();
+console.log('Did ' + userName + ' guess that I am a UW alumnus? ' + jeremySchool);
 
 if(jeremySchool === 'Y' || jeremySchool === 'YES') {
-    alert('Yep! I moved to Washington and attended the Full-time MBA program at UW - GO DAWGS! Let\'s keep going.');
+    alert('Yep! Nice job, ' + userName + '. I moved to Washington and attended the Full-time MBA program at UW - GO DAWGS! Let\'s keep going.');
 } else if (jeremySchool === 'N' || jeremySchool === 'NO') {
-    alert('Even though I didn\'t grow up here, I moved to Washington and attended the MBA program at UW - GO DAWGS! Let\'s keep going.');
+    alert( 'Well, ' + userName + ', even though I didn\'t grow up here, I moved to Washington and attended the MBA program at UW - GO DAWGS! Let\'s keep going.');
 } else {
-    alert('Hmm... not answering tells me you must be a Washington State fan. I did my MBA at UW before starting at Code Fellows- GO DAWGS! Let\'s keep going.');
+    alert(userName + '... not answering tells me you must be a Washington State fan. I did my MBA at UW before starting at Code Fellows- GO DAWGS! Let\'s keep going.');
 }
 
-var jeremyHobbies =  prompt('Ok so I\'m a 33 year old PNW transplant that attended UW. But I actually moved here for another reason... is that reason because I love outdoor activities? Reply Y or N.').toUpperCase();
-console.log("User entered " + jeremyHobbies);
+var jeremyHobbies =  prompt('Ok so I\'m a 33 year old PNW transplant that attended UW. But ' + userName + ', I actually moved here for another reason... is that reason because I love outdoor activities? Reply Y or N.').toUpperCase();
+console.log('Did ' + userName + ' guess that I enjoy outdoor activities? ' + jeremyHobbies);
 
 if(jeremyHobbies === 'Y' || jeremyHobbies === 'YES') {
-    alert('Haha you caught me! School was ok but I really moved here so I could go snowboarding and backpacking in the beautiful PNW. One more question to go!');
+    alert('Haha ' + userName + ', you caught me! School was ok but I really moved here so I could go snowboarding and backpacking in the beautiful PNW. Onwards!');
 } else if (jeremyHobbies === 'N' || jeremyHobbies === 'NO') {
-    alert('I appreciate the vote of confidence, but truthfully I moved here so I could spend all my free time snowboarding and backpacking. One more question to go!');
+    alert('I appreciate the vote of confidence ' + userName + ', but truthfully I moved here so I could spend all my free time snowboarding and backpacking. Onwards!');
 } else {
-    alert('I get it...I\'d rather be snowboarding or backpacking than answering these questions, too. Only one more to go!');
+    alert('I totally get it ' + userName + '...I\'d rather be snowboarding or backpacking than answering these questions, too. Onwards!');
 }
 
-var jeremyDog =  prompt('Ok so I\'m a 33 year old PNW transplant that attended UW and loves the outdoors. Final question: is my dog/best friend\'s name Bruce? Reply Y or N.').toUpperCase();
-console.log("User entered " + jeremyHobbies);
+var jeremyPet =  prompt('Ok ' + userName + ', so I\'m a 33 year old PNW transplant that attended UW and loves the outdoors. Final question: Do I have any pets? Reply Y or N.').toUpperCase();
+console.log('What did ' + userName + ' guess about my pet ownership? '+ jeremyPet);
 
-if(jeremyDog === 'Y' || jeremyDog === 'YES') {
-    alert('Trick question! I don\'t have any pets, which is a total bummer I know. Thanks for taking my quiz! Click OK to learn more about me.');
-} else if (jeremyDog === 'N' || jeremyDog === 'NO') {
-    alert('Trick question! I don\'t have any pets, which is a total bummer I know. Thanks for taking my quiz! Click OK to learn more about me.');
+if(jeremyPet === 'Y' || jeremyPet === 'YES') {
+    alert('Unfortunately you\'re wrong, I don\'t have any pets which is a total bummer.\ ' + userName + ', thanks for taking my quiz! Click OK to learn more about me.');
+} else if (jeremyPet === 'N' || jeremyPet === 'NO') {
+    alert('Unfortunately you\'re right, I don\'t have any pets which is a total bummer.\ ' + userName + ', thanks for taking my quiz! Click OK to learn more about me.');
 } else {
-    alert('You got it right! Trick question, I don\'t have any pets, which is a total bummer I know. Thanks for taking my quiz! Click OK to learn more about me.');
+    alert('Hmm... ' + userName + ' must actually be a cat walking on a keyboard. Thanks Mr. Mittens for taking my quiz! Click OK to learn more about me.');
 }
